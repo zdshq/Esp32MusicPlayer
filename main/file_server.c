35c531357e1c9ac9fe94e37b3352eb44f8f69eda
zdshq <1805300624@qq.com>
2022-09-14 20:38:09 +0800

    master

--- main/file_server.c
+++ main/file_server.c
@@ -179,7 +179,7 @@ static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filena
 static const char* get_path_from_uri(char *dest, const char *base_path, const char *uri, size_t destsize)
 {
     const size_t base_pathlen = strlen(base_path);
-    size_t pathlen = strlen(uri);
+    size_t pathlen = strlen(uri);  
 
     const char *quest = strchr(uri, '?');
     if (quest) {
